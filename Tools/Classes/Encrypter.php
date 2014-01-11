50e8404cd685d33c98dc9894e59a0c79a7343de9
rudak <roiduboul@gmail.com>
2014-01-12 00:28:44 +0100

    ajout encrypter

---
+++ Tools/Classes/Encrypter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Perso\Tools\Classes;
+
+/**
+ * Petite methode statique permetant d'encrypter un plainpassword
+ *
+ * @author Rudak
+ */
+class Encrypter {
+
+    static $iterations = 1583;
+
+    static function encrypt($plainPassword, $salt) {
+        $i = 0;
+        $out = '';
+        do {
+            $out .= sha1($out.$plainPassword . $i . $salt . $out);
+        } while (++$i < self::$iterations);
+
+        return substr($out,0,20);
+    }
+
+}
