3d31740ab5f5bf4c3ec201437f69f9cc64976054
rudak <roiduboul@gmail.com>
2014-03-04 10:27:23 +0100

    modif controller

--- Controller/MyController.php
+++ Controller/MyController.php
@@ -6,5 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MyController extends Controller
 {
-    
+
+    private $Em;
+
+    protected function setFlashMessage($message, $type = 'info')
+    {
+        $this->get('session')->getFlashBag()->add(
+                $type, $message
+        );
+    }
+
+    /**
+     * Récupération de l'entity manager
+     * @return type
+     */
+    protected function getEm()
+    {
+        if (null === $this->Em) {
+            $this->Em = $this->getDoctrine()->getManager();
+        }
+        return $this->Em;
+    }
+
+    /**
+     * Récupération d'un repo
+     */
+    protected function getRepo($repositoryName)
+    {
+        return $this->getEm()->getRepository($repositoryName);
+    }
+
 }
